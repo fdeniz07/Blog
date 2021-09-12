e02ea8eff09133ffa6652a00d5790326a2d6a3b2
Fatih Deniz <deniz.fatih@hotmail.de>
2021-09-12 05:59:12 +0200

    Day 4 - Results

---
+++ CoreLayer/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CoreLayer.Utilities.Results.ComplexTypes;
+
+namespace CoreLayer.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; } //ResultStatus.Success // ResultStatus.Error
+        public string Message { get;}
+        public Exception Exception { get;}
+    }
+}
---
+++ CoreLayer/Utilities/Results/ComplexTypes/ResultStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreLayer.Utilities.Results.ComplexTypes
+{
+    public enum ResultStatus
+    {
+        Success = 0,
+        Error = 1,
+        Warning = 2, // ResultStatus.Warning
+        Info = 3
+    }
+}
