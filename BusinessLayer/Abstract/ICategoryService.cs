f67e2afee5cb8285c5d9285e4b220c1d841dcf2c
Fatih Deniz <deniz.fatih@hotmail.de>
2021-09-13 22:47:11 +0200

    Day 5

--- BusinessLayer/Abstract/ICategoryService.cs
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -10,6 +10,7 @@ namespace BusinessLayer.Abstract
     {
         Task<IDataResult<Category>> Get(int categoryId);
         Task<IDataResult<IList<Category>>> GetAll();
+        Task<IDataResult<IList<Category>>> GetAllByNonDeleted();
         Task<IResult> Add(CategoryAddDto categoryAddDto,string createdByName);
         Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
         Task<IResult> Delete(int categoryId);
---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using CoreLayer.Utilities.Results.Abstract;
+using CoreLayer.Utilities.Results.ComplexTypes;
+using CoreLayer.Utilities.Results.Concrete;
+using DataAccessLayer.Abstract.UnitOfWorks;
+using EntityLayer.Concrete;
+using EntityLayer.Dtos;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region Implementation of ICategoryService
+
+        public async Task<IDataResult<Category>> Get(int categoryId)
+        {
+            var category = await _unitOfWork.Categories.GetAsync(c => c.Id == categoryId, c => c.Blogs);
+            if (category != null)
+            {
+                return new DataResult<Category>(ResultStatus.Success, category);
+            }
+
+            return new DataResult<Category>(ResultStatus.Error, "Böyle bir kategori bulunamadi.", null);
+        }
+
+        public async Task<IDataResult<IList<Category>>> GetAll()
+        {
+            var categories = await _unitOfWork.Categories.GetAllAsync(null, c => c.Blogs);
+
+            if (categories.Count > -1) //Hic kategorisi de olmayabilir. O yüzden 0 yerine -1 yaziyoruz
+            {
+                return new DataResult<IList<Category>>(ResultStatus.Success, categories);
+            }
+
+            return new DataResult<IList<Category>>(ResultStatus.Error, "Hic bir kategori bulunamadi.", null);
+        }
+
+        public Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Delete(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> HardDelete(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IDataResult<IList<Category>>> GetAllByNonDeleted() //Silinmemis olanlari getir
+        {
+            var categories = await _unitOfWork.Categories.GetAllAsync(c => !c.IsDeleted, c => c.Blogs);
+
+            if (categories.Count > -1) //Hic kategorisi de olmayabilir. O yüzden 0 yerine -1 yaziyoruz
+            {
+                return new DataResult<IList<Category>>(ResultStatus.Success, categories);
+            }
+
+            return new DataResult<IList<Category>>(ResultStatus.Error, "Hic bir kategori bulunamadi.", null);
+        }
+
+        #endregion
+    }
+}
